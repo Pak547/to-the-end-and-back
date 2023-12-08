6fcb3f05fe653603131e357c930ee189fdb875c1
Pak <pakapongm@gmail.com>
2023-12-07 18:26:48 -0800

    soft changes to format and delete comments

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const db = require('../db/db.json');
-const { readFile } = require('node:fs');
+const { readFile } = require('fs');
 
-router.get('/notes', (req,res) => {
-   //fs readFile
-   fs.readFile(db)
+router.get('/notes', (req, res) => {
+   fs.readFile(db);
 });
 module.exports = router;
